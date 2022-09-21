4e7078c1e91809ad157b9ec6c5492276546cd893
SARVAGNA! <79441803+SARVAGNA-7077@users.noreply.github.com>
2022-09-21 08:18:00 -0700

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// document.write("Value of PI"+Math.PI+"<br>");
-// document.write("Random value generated:"+Math.random()+"<br>");
-// document.write("Rounded value of 69:"+Math.round(69)+"<br>");
-// document.write("Value of 6<SUP>2</SUP>:"+Math.pow(6,2)+"<br>");
-// document.write("Square root of 4761:69<br>");
